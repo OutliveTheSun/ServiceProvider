065cd77dcf1789e4534ebb12ab86afae5726e91c
vtewes <verena.tewes@gmx.net>
2021-11-23 17:50:22 +0100

    Version 1.0.10 - Switch to java version 8 and fix for getting package name

--- serviceProvider/src/main/java/com/outlivethesun/serviceprovider/internal/classloader/ReflectionsInfo.kt
+++ serviceProvider/src/main/java/com/outlivethesun/serviceprovider/internal/classloader/ReflectionsInfo.kt
@@ -8,7 +8,7 @@ internal class ReflectionsInfo {
 
     fun <A : Any> findImplementingClassesOfInterface(
         interfaceName: KClass<A>,
-        packagePath: String = interfaceName.java.packageName
+        packagePath: String = interfaceName.java.`package`.name
     ): List<KClass<out A>> {
         val reflections = Reflections(packagePath, SubTypesScanner())
         return reflections.getSubTypesOf(interfaceName.java).map { it.kotlin }
