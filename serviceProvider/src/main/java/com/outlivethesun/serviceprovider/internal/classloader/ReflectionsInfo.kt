22ca683caa9a902949808b66a322501d02e4aba3
vtewes <verena.tewes@gmx.net>
2022-01-18 17:37:32 +0100

    Version 1.0.15 - Cache default reflections

--- serviceProvider/src/main/java/com/outlivethesun/serviceprovider/internal/classloader/ReflectionsInfo.kt
+++ serviceProvider/src/main/java/com/outlivethesun/serviceprovider/internal/classloader/ReflectionsInfo.kt
@@ -6,23 +6,41 @@ import kotlin.reflect.KClass
 
 class ReflectionsInfo {
 
+    private lateinit var defaultReflections: Reflections
+
     fun <A : Any> findImplementingClassesOfInterface(
         interfaceName: KClass<A>,
         packagePath: String = ""
     ): List<KClass<out A>> {
-        var path = packagePath
-        if (path.isEmpty()) {
-            val firstStackElement = Thread.currentThread().stackTrace.last()
-            checkIsValidPackageName(interfaceName, firstStackElement)
-            path = firstStackElement.className.substringBefore(".")
+        val reflections = if (packagePath.isEmpty()) {
+            fetchDefaultReflections(interfaceName)
+        } else {
+            createReflections(packagePath)
         }
-        val reflections = Reflections(path, Scanners.SubTypes)
         return reflections.getSubTypesOf(interfaceName.java).map { it.kotlin }
+    }
+
+    private fun <A : Any> fetchDefaultReflections(interfaceName: KClass<A>): Reflections {
+        return if (!this::defaultReflections.isInitialized) {
+            createReflections(determinePathFromStack(interfaceName)).apply { defaultReflections = this }
+        } else {
+            defaultReflections
+        }
+    }
+
+    private fun createReflections(path: String): Reflections {
+        return Reflections(path, Scanners.SubTypes)
     }
 
     private fun <A : Any> checkIsValidPackageName(interfaceName: KClass<A>, stackElement: StackTraceElement) {
         if (!stackElement.className.contains(".")) {
             throw ReflectionInfoMissingPackageException(interfaceName.simpleName!!, stackElement.fileName)
         }
+    }
+
+    private fun <A : Any> determinePathFromStack(interfaceName: KClass<A>): String {
+        val firstStackElement = Thread.currentThread().stackTrace.last()
+        checkIsValidPackageName(interfaceName, firstStackElement)
+        return firstStackElement.className.substringBefore(".")
     }
 }
--- serviceProvider/src/test/java/com/outlivethesun/serviceprovider/internal/classloader/ReflectionsInfoTest.kt
+++ serviceProvider/src/test/java/com/outlivethesun/serviceprovider/internal/classloader/ReflectionsInfoTest.kt
@@ -2,16 +2,57 @@ package com.outlivethesun.serviceprovider.internal.classloader
 
 import com.outlivethesun.serviceprovider.IService
 import com.outlivethesun.serviceprovider.Service
+import io.mockk.*
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.reflections.Reflections
+import org.reflections.scanners.Scanners
+import kotlin.reflect.KClass
 
 internal class ReflectionsInfoTest{
 
+    companion object{
+//        const val packagePath = "unsinn"
+        const val packagePath = "com.outlivethesun"
+    }
+
+    private val resultSet : Set<Class<out IService>> = setOf()
     private val reflectionsInfo = ReflectionsInfo()
+
+    init {
+        mockkConstructor(Reflections::class)
+//        every { constructedWith<Reflections>(OfTypeMatcher<KClass<IService>>(KClass::class), EqMatcher("packagePath")) }
+        every { constructedWith<Reflections>(EqMatcher(packagePath), EqMatcher(Scanners.SubTypes)).getSubTypesOf(IService::class.java) } returns resultSet
+    }
 
     @Test
     fun findImplementingClassesOfInterface(){
+        val implementingClasses = reflectionsInfo.findImplementingClassesOfInterface(IService::class, packagePath)
+        assertClassFound(implementingClasses)
+    }
+
+    @Test
+    fun findImplementingClassesOfInterfaceDefaultPath(){
         val implementingClasses = reflectionsInfo.findImplementingClassesOfInterface(IService::class)
+        assertClassFound(implementingClasses)
+    }
+
+    @Test
+    fun findImplementingClassesOfInterfaceDefaultPathCached(){
+        reflectionsInfo.findImplementingClassesOfInterface(IService::class)
+        val implementingClasses = reflectionsInfo.findImplementingClassesOfInterface(IService::class)
+        assertClassFound(implementingClasses)
+    }
+
+    @Test
+    fun findImplementingClassesOfInterfaceUnmocked(){
+        unmockkAll()
+        reflectionsInfo.findImplementingClassesOfInterface(IService::class)
+        val implementingClasses = reflectionsInfo.findImplementingClassesOfInterface(IService::class)
+        assertClassFound(implementingClasses)
+    }
+
+    private fun assertClassFound(implementingClasses: List<KClass<*>>) {
         assertEquals(1, implementingClasses.size)
         assertEquals(Service::class, implementingClasses.first())
     }
