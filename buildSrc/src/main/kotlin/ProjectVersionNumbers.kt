59b80ae62cac344234ad0fc4c97ea7948261248c
acoria <tewes.verena@gmail.com>
2022-05-10 16:58:58 +0200

    Update to buildSrcVersion 1.0.0

--- buildSrc/src/main/kotlin/ProjectVersionNumbers.kt
+++ buildSrc/src/main/kotlin/ProjectVersionNumbers.kt
@@ -3,6 +3,7 @@
  * TO NOT ADD YOUR OWN HERE -> use [ModuleVersionNumbers]
  */
 object ProjectVersionNumbers {
+    //buildSrcVersion: 1.0.0
     const val kotlin = "1.6.21"
     const val kotlinCompileOnly = "1.6"
     const val mockk = "1.12.2"
